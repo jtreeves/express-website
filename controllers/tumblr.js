a94e155ae56934b585433cdbe0c3dd286cca32d7
jtreeves <jackson.t.reeves@gmail.com>
2021-02-04 11:58:10 -0500

    [tumblr] Add logs to dig into returned data

--- controllers/tumblr.js
+++ controllers/tumblr.js
@@ -11,6 +11,14 @@ const tumblrUrl = 'https://api.tumblr.com/v2/blog/programming10101.tumblr.com/po
 router.get('/', async (req, res) => {
     try {
         const result = await axios.get(tumblrUrl + key)
+        console.log(`RESULT: ${result}`)
+        console.log(`RESULT KEYS: ${Object.keys(result)}`)
+        console.log(`RESULT.DATA: ${result.data}`)
+        console.log(`RESULT.DATA KEYS: ${Object.keys(result.data)}`)
+        console.log(`RESULT.DATA.RESPONSE: ${result.data.response}`)
+        console.log(`RESULT.DATA.RESPONSE KEYS: ${Object.keys(result.data.response)}`)
+        console.log(`RESULT.DATA.RESPONSE.POSTS: ${result.data.response.posts}`)
+        console.log(`RESULT.DATA.RESPONSE.POSTS KEYS: ${Object.keys(result.data.response.posts)}`)
         res.status(200).json({posts: result.data.response.posts})
     } catch (error) {
         res.status(400).json({msg: error})
