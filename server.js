012cf4789bb1721c80a74600328af34831a1f6d4
jtreeves <jackson.t.reeves@gmail.com>
2021-02-03 23:47:25 -0500

    [server] Comment out tumblr and github pages to allow twitter testing

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const app = express()
 const cors = require('cors')
 
 const twitter = require('./controllers/twitter')
-const tumblr = require('./controllers/tumblr')
-const github = require('./controllers/github')
+// const tumblr = require('./controllers/tumblr')
+// const github = require('./controllers/github')
 
 app.use(cors())
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use('/twitter', twitter)
-app.use('/tumblr', tumblr)
-app.use('/github', github)
+// app.use('/tumblr', tumblr)
+// app.use('/github', github)
 
 app.get('/', (req, res) => {
     res.status(200).json({msg: 'Viewing my backend'})
