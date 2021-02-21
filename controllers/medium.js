4f0016f3be1cb716a971d280cff63b430ee007e7
jtreeves <jackson.t.reeves@gmail.com>
2021-02-21 02:35:48 -0500

    [medium] Add module to parse XML output into JSON

--- controllers/medium.js
+++ controllers/medium.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const axios = require('axios')
+const Parser = require('rss-parser')
+const parser = new Parser()
 
 const router = express.Router()
 
@@ -8,8 +10,17 @@ const mediumUrl = 'https://medium.com/feed/@jtreeves'
 
 router.get('/', async (req, res) => {
     try {
-        const result = await axios.get(mediumUrl)
-        res.status(200).json({posts: JSON.stringify(result)})
+        const result = await parser.parseURL(mediumUrl)
+        // const result = await axios.get(mediumUrl)
+        console.log(`RESULT: ${result}`)
+        console.log(`RESULT KEYS: ${Object.keys(result)}`)
+        console.log(`RESULT.ITEMS: ${result.items}`)
+        console.log(`RESULT.ITEMS KEYS: ${Object.keys(result.items)}`)
+        // const resultJSON = parser.parseString(result)
+        // console.log(`RESULTJSON: ${resultJSON}`)
+        // console.log(`RESULTJSON KEYS: ${Object.keys(resultJSON)}`)
+        // console.log(`RESULT JSON: ${JSON.stringify(result)}`)
+        res.status(200).json({posts: result})
     } catch (error) {
         res.status(400).json({msg: error})
     }
