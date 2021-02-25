719ced9f93bdb27e74e54a480ab534fae9e3e83d
jtreeves <jackson.t.reeves@gmail.com>
2021-02-25 13:27:15 -0500

    [tumblr] Allow handling both post type and tag content by using params

--- controllers/tumblr.js
+++ controllers/tumblr.js
@@ -6,11 +6,15 @@ const router = express.Router()
 
 const key = process.env.TUMBLR_API_KEY
 
-const tumblrUrl = 'https://api.tumblr.com/v2/blog/jacksonreeves.tumblr.com/posts/link?tag=resources&api_key=' + key
+const tumblrUrl = 'https://api.tumblr.com/v2/blog/jacksonreeves.tumblr.com/posts/'
 
-router.get('/', async (req, res) => {
+router.get('/:type/:tag', async (req, res) => {
+    const type = req.params.type
+    const tag = req.params.tag
     try {
-        const result = await axios.get(tumblrUrl)
+        const result = await axios.get(
+            tumblrUrl + type + '?tag=' + tag + '&api_key=' + key
+        )
         res.status(200).json({posts: result.data.response.posts})
     } catch (error) {
         res.status(400).json({msg: error})
